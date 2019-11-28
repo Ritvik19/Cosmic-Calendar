b44abe229bdd4de31cefe5c9154635214df398ec
Ritvik19 <rastogiritvik99@gmail.com>
2019-11-28 20:35:38 +0530

    Additional Data

--- js/main.js
+++ js/main.js
@@ -27,13 +27,19 @@ function windowResizeFunction() {
         $("#mob").show();
         $("main")[0].style.height = "auto";
         $("main")[0].style.overflowY = "visible";
+        $("aside")[0].style.height = "auto";
+        $("aside")[0].style.overflowY = "visible";
         for (var i = 0; i < ListElements.length; i++)
             ListElements[i].classList.remove("mystyle");
+        // $("body").style.height = "auto";
     } else {
         $(".collection-container").show();
         $("#mob").hide();
         $("main")[0].style.height = "100vh";
         $("main")[0].style.overflowY = "scroll";
+        $("aside")[0].style.height = "100vh";
+        $("aside")[0].style.overflowY = "scroll";
+        // $("body").style.height = "98vh";
     }
 }
 
@@ -147,6 +153,42 @@ $.getJSON("data/05.json", function(json) {
     }
     document
         .getElementById("d05")
+        .getElementsByTagName("table")[0]
+        .getElementsByTagName("tbody")[0].innerHTML = contents;
+});
+
+$.getJSON("data/06.json", function(json) {
+    contents = "";
+    for (i = 0; i < json.Date.length; i++) {
+        contents +=
+            "<tr> <td>" +
+            json.Date[i] +
+            "</td> <td>" +
+            json.Years[i] +
+            "</td> <td>" +
+            json.Event[i] +
+            "</td> </tr>";
+    }
+    document
+        .getElementById("d06")
+        .getElementsByTagName("table")[0]
+        .getElementsByTagName("tbody")[0].innerHTML = contents;
+});
+
+$.getJSON("data/07.json", function(json) {
+    contents = "";
+    for (i = 0; i < json.Date.length; i++) {
+        contents +=
+            "<tr> <td>" +
+            json.Date[i] +
+            "</td> <td>" +
+            json.Years[i] +
+            "</td> <td>" +
+            json.Event[i] +
+            "</td> </tr>";
+    }
+    document
+        .getElementById("d07")
         .getElementsByTagName("table")[0]
         .getElementsByTagName("tbody")[0].innerHTML = contents;
 });
