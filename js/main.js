8cc8b903ce0e0c56085fdb6421090d7c3aaac7ca
Ritvik19 <rastogiritvik99@gmail.com>
2020-01-02 19:03:20 +0530

    UI Modified

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-// active on click
-var UnorderedList = document.getElementsByTagName("ul")[0];
-var ListElements = UnorderedList.getElementsByTagName("a");
+$(document).ready(function() {
+    $('.sidenav').sidenav();
+});
 
 //smooth scroll
 $(document).ready(function() {
@@ -20,144 +20,10 @@ $(document).ready(function() {
     });
 });
 
-// window resizing
-function windowResizeFunction() {
-    if (screen.width < 600) {
-        $(".collection-container").hide();
-        $("#mob").show();
-        $("main")[0].style.height = "auto";
-        $("main")[0].style.overflowY = "visible";
-        $("aside")[0].style.height = "auto";
-        $("aside")[0].style.overflowY = "visible";
-        for (var i = 0; i < ListElements.length; i++)
-            ListElements[i].classList.remove("mystyle");
-        // $("body").style.height = "auto";
-    } else {
-        $(".collection-container").show();
-        $("#mob").hide();
-        $("main")[0].style.height = "100vh";
-        $("main")[0].style.overflowY = "scroll";
-        $("aside")[0].style.height = "100vh";
-        $("aside")[0].style.overflowY = "scroll";
-        // $("body").style.height = "98vh";
-    }
-}
+data = [data1, data2, data3, data4, data5, data6, data7]
 
-$("#mob").click(function() {
-    $(".collection-container").toggle(1000);
-});
-
-window.addEventListener("resize", windowResizeFunction);
-windowResizeFunction();
-
-// sticky menu
-function sticky_relocate() {
-    var window_top = $(window).scrollTop();
-    var div_top = $("#sticky-anchor").offset().top;
-    if (window_top > div_top) {
-        $("#sticky").addClass("stick");
-    } else {
-        $("#sticky").removeClass("stick");
-    }
-}
-
-$(function() {
-    $(window).scroll(sticky_relocate);
-    sticky_relocate();
-});
-
-// data
-$.getJSON("data/01.json", function(json) {
-    contents = "";
-    for (i = 0; i < json.Date.length; i++) {
-        contents +=
-            "<tr> <td>" +
-            json.Date[i] +
-            "</td> <td>" +
-            json.YearsAgo[i] +
-            "</td> <td>" +
-            json.Event[i] +
-            "</td> </tr>";
-    }
-    document
-        .getElementById("d01")
-        .getElementsByTagName("table")[0]
-        .getElementsByTagName("tbody")[0].innerHTML = contents;
-});
-
-$.getJSON("data/02.json", function(json) {
-    contents = "";
-    for (i = 0; i < json.Date.length; i++) {
-        contents +=
-            "<tr> <td>" +
-            json.Date[i] +
-            "</td> <td>" +
-            json.YearsAgo[i] +
-            "</td> <td>" +
-            json.Event[i] +
-            "</td> </tr>";
-    }
-    document
-        .getElementById("d02")
-        .getElementsByTagName("table")[0]
-        .getElementsByTagName("tbody")[0].innerHTML = contents;
-});
-
-$.getJSON("data/03.json", function(json) {
-    contents = "";
-    for (i = 0; i < json.Date.length; i++) {
-        contents +=
-            "<tr> <td>" +
-            json.Date[i] +
-            "</td> <td>" +
-            json.YearsAgo[i] +
-            "</td> <td>" +
-            json.Event[i] +
-            "</td> </tr>";
-    }
-    document
-        .getElementById("d03")
-        .getElementsByTagName("table")[0]
-        .getElementsByTagName("tbody")[0].innerHTML = contents;
-});
-
-$.getJSON("data/04.json", function(json) {
-    contents = "";
-    for (i = 0; i < json.Date.length; i++) {
-        contents +=
-            "<tr> <td>" +
-            json.Date[i] +
-            "</td> <td>" +
-            json.YearsAgo[i] +
-            "</td> <td>" +
-            json.Event[i] +
-            "</td> </tr>";
-    }
-    document
-        .getElementById("d04")
-        .getElementsByTagName("table")[0]
-        .getElementsByTagName("tbody")[0].innerHTML = contents;
-});
-
-$.getJSON("data/05.json", function(json) {
-    contents = "";
-    for (i = 0; i < json.Date.length; i++) {
-        contents +=
-            "<tr> <td>" +
-            json.Date[i] +
-            "</td> <td>" +
-            json.YearsAgo[i] +
-            "</td> <td>" +
-            json.Event[i] +
-            "</td> </tr>";
-    }
-    document
-        .getElementById("d05")
-        .getElementsByTagName("table")[0]
-        .getElementsByTagName("tbody")[0].innerHTML = contents;
-});
-
-$.getJSON("data/06.json", function(json) {
+for (idx = 1; idx <= 7; idx++) {
+    json = data[idx - 1];
     contents = "";
     for (i = 0; i < json.Date.length; i++) {
         contents +=
@@ -170,25 +36,7 @@ $.getJSON("data/06.json", function(json) {
             "</td> </tr>";
     }
     document
-        .getElementById("d06")
+        .getElementById("d-0" + idx)
         .getElementsByTagName("table")[0]
         .getElementsByTagName("tbody")[0].innerHTML = contents;
-});
-
-$.getJSON("data/07.json", function(json) {
-    contents = "";
-    for (i = 0; i < json.Date.length; i++) {
-        contents +=
-            "<tr> <td>" +
-            json.Date[i] +
-            "</td> <td>" +
-            json.Years[i] +
-            "</td> <td>" +
-            json.Event[i] +
-            "</td> </tr>";
-    }
-    document
-        .getElementById("d07")
-        .getElementsByTagName("table")[0]
-        .getElementsByTagName("tbody")[0].innerHTML = contents;
-});
+}
